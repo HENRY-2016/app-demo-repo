a6215e393f38bb8559637b1bb6101bc88661b01c
Henry-2016 <jacob2016henry@gmail.com>
2020-09-20 13:44:05 -0400

    20-09-2020:1:41

---
+++ static/js/main.js
@@ -0,0 +1,11 @@
+
+
+function  ShowAppName ()
+{
+cordova.getAppVersion.getAppName ( function (appname) {alert(appname)})
+}
+
+function  ShowAppVersion ()
+{
+cordova.getAppVersion.getAppVersionCode ( function (vercode) {alert(vercode)})
+}
